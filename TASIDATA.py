32a92635c7b28ff4c6d1847c0b3e1813a1810188
Alsaif <mozr2727@gmail.com>
2023-07-23 23:21:01 +0300

    Update TASIDATA.py

--- TASIDATA.py
+++ TASIDATA.py
@@ -4,13 +4,14 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import sqlalchemy
-
+# URL source
 url = 'https://www.saudiexchange.sa/wps/portal/saudiexchange/ourmarkets/main-market-watch'
+
+# initate WebDriver and get the targeted website URl
 driver = webdriver.Chrome(service=Service('/Users/saif/Downloads/chromedriver_mac64/chromedriver'))
 driver.get(url)
 
-#Scrapping Parts
- 
+#Scrapping Parts, different desired parts of the table. They are located using their XPATHs
 TickersID = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[1]')
 Prices = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[2]')
 TradeChangeValue =  driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[4]')
@@ -20,8 +21,11 @@ VolumesTraded = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollB
 Open = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[8]')
 High = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[9]')
 Low = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[10]')
+
+# Transferring data into a dataframe using Panda
 df = pd.DataFrame(columns =["TickersID", "Price", "Trade Change Value", "Trade Change Percentage", "Number of Trades","Volume Traded","Open","High","Low"])
 for i in range(len(TickersID)):
     df = df._append({'TickersID': TickersID[i].text, 'Price':Prices[i].text, 'Trade Change Value': TradeChangeValue[i].text, 'Trade Change Percentage': TradeChangePercentage[i].text, 'Number of Trades': NumberOfTrades[i].text, 'Volume Traded': VolumesTraded[i].text,'Open': Open[i].text, 'High': High[i].text, 'Low': Low[i].text}, ignore_index = True)
 
+# Exporting the dataframe into an index format JSON file named "TasiExtract".
 df.to_json(orient='index', path_or_buf="TasiExtract.json")
