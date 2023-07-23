dba20743826cdb668b41a69ba28367e37cf4a3e7
Alsaif <mozr2727@gmail.com>
2023-07-23 23:12:28 +0300

    Update TASIDATA.py

--- TASIDATA.py
+++ TASIDATA.py
@@ -1,6 +1,27 @@
-import requests
-from bs4 import BeautifulSoup
-url = 'https://www.saudiexchange.sa/wps/portal/saudiexchange/ourmarkets/main-market-watch/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8ziTR3NDIw8LAz8LVxcnA0C3bwtPLwM_I0MzMz1w1EVGAQHmAIVBPga-xgEGbgbmOlHEaPfAAdwNCCsPwpNia-7mUGgn2Ogv5G5qYFBsBG6AixOBCvA44bgxCL9gtzQCIPMgHQAVrLIOQ!!/dz/d5/L0lHSkovd0RNQUZrQUVnQSEhLzROVkUvZW4!/'
-page = requests.get(url)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import sqlalchemy
 
-print(page.status_code)
+url = 'https://www.saudiexchange.sa/wps/portal/saudiexchange/ourmarkets/main-market-watch'
+driver = webdriver.Chrome(service=Service('/Users/saif/Downloads/chromedriver_mac64/chromedriver'))
+driver.get(url)
+
+#Scrapping Parts
+ 
+TickersID = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[1]')
+Prices = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[2]')
+TradeChangeValue =  driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[4]')
+TradeChangePercentage =  driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[5]')
+NumberOfTrades =  driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[6]')
+VolumesTraded = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[7]')
+Open = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[8]')
+High = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[9]')
+Low = driver.find_elements(By.XPATH, '//div[@class="dataTables_scrollBody"]//tbody/tr//td[10]')
+df = pd.DataFrame(columns =["TickersID", "Price", "Trade Change Value", "Trade Change Percentage", "Number of Trades","Volume Traded","Open","High","Low"])
+for i in range(len(TickersID)):
+    df = df._append({'TickersID': TickersID[i].text.rsplit('\r',1)[1], 'Price':Prices[i].text, 'Trade Change Value': TradeChangeValue[i].text, 'Trade Change Percentage': TradeChangePercentage[i].text, 'Number of Trades': NumberOfTrades[i].text, 'Volume Traded': VolumesTraded[i].text,'Open': Open[i].text, 'High': High[i].text, 'Low': Low[i].text}, ignore_index = True)
+
+df.to_json(orient='index', path_or_buf="TasiExtract.json")
